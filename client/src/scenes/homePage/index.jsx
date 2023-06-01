efbf81f56f5d344ac082c8cf4e43b8b667a18caa
Folk_Jrk <jarmekorntec@gmail.com>
2023-06-01 18:35:34 +0100

    forntend part 1_ folk
    navbar , login and register

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,11 @@
-const HomePage =() =>{
-    return(<div>homepage</div>)
+import { Box } from "@mui/material";
+import Navbar from 'scenes/navbar/index.jsx';
+
+const HomePage =() => {
+    return( <Box>
+        < Navbar />
+    </Box>);
+    
 };
 
 export default HomePage;
