6e9592f87706a96a924bafced9e0103652bed218
Ukasha Noor <ukashanoor.iiitk@gmail.com>
2023-06-08 16:24:33 +0100

    Added Event mongoose Model

---
+++ server/models/Event.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const eventSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    eventName: {
+      type: String,
+      required: true,
+    },
+    eventLocation: {
+        type: String,
+        required: true,
+    },
+    eventDate: {
+        type: Date,
+        required: true
+    },
+    eventDescription: {
+      type: String
+    },
+    organizerName: {
+        type: String,
+        required: true
+    },
+    organizerPhone: {
+        type: String,
+        required: true
+    },
+    donationCategory: {
+        type: Array,
+        required: true
+    },
+    donationType: {
+        type: String,
+        required: true
+    },
+    volunteerOpportunity: String,
+    supplies: String,
+    paymentMethod: String
+  },
+  { timestamps: true }
+);
+
+const Event = mongoose.model("Event", postSchema);
+
+export default Event;
