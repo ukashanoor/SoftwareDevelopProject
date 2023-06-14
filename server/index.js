e8ffe91d845cbfa5e295137dc0a489239be61178
ukashanoor <ukasha.noor@bigyellowfish.io>
2023-06-14 16:40:45 +0100

    Add APIs for Donation functionality

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import donationRoutes from "./routes/donations.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
@@ -56,6 +57,7 @@ app.post("/posts", verifyToken, upload.fields([
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+app.use("/donations", donationRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
